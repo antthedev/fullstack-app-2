65d9f3c6ec129d1d3d5c9cab95aef5bf5705c46d
Antonie Smith <61763868+antthedev@users.noreply.github.com>
2022-11-07 00:16:14 -0500

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+ // config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://antTheDev:Cocopuffs12@cluster0.ypsp3o5.mongodb.net/?retryWrites=true&w=majority', 
+    'dbName': 'Project list' 
+};
